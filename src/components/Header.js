c71468543b2f8e5f5a1bb5d40a08b8f778da77b7
Dennis Kim <dennis.kim@hotmail.com>
2022-09-16 18:23:51 -0700

    add mixins and app name to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,22 @@ import Navigation from "./Navigation"
 import { useState } from "react";
 import { HiMenu } from "react-icons/hi";
 import { BiMenuAltLeft } from "react-icons/bi";
+import { NavLink } from "react-router-dom";
 const Header = () => {
   const [ isSelected, setIsSelected ] = useState(true);
-  const navigationHandler = (msg) => {
-    console.log(msg);
+  const navigationHandler = () => {
     setIsSelected(!isSelected);
   }
   console.log(isSelected);
   return (
     <header className="header">
-        <HiMenu className={`hamburger ${ isSelected ? 'active' : 'inactive' }`} onClick={ () => navigationHandler('closed menu')}/>
-        <BiMenuAltLeft onClick={ () => navigationHandler('opened menu')} className={`hamburger ${ isSelected ? 'inactive': null }`}/>
+        <div className="header-bgc">
+          <NavLink to='/' className='app-heading'>
+            <h1>The Stoic App</h1>
+          </NavLink>
+        </div>
+        <HiMenu className={`hamburger ${ isSelected ? 'active' : 'inactive' }`} onClick={ navigationHandler}/>
+        <BiMenuAltLeft onClick={ navigationHandler} className={`hamburger ${ isSelected ? 'inactive': null }`}/>
         <Navigation isSelected={isSelected}/>
     </header>
   )
