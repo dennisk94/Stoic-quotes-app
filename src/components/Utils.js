b3b3b79a82a4007b8d92dd01a43cb7fee6774d50
Dennis Kim <dennis.kim@hotmail.com>
2022-09-18 20:44:02 -0700

    add styling for copy to clipboard component

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,9 +1,14 @@
 import { AiOutlineCopy } from "react-icons/ai";
+import { AiFillCopy } from "react-icons/ai";
 
-const Utils = ( { handleCopyText } ) => {
+const Utils = ( { handleCopyText, isCopied } ) => {
   return (
     <div className="utils">
-        <AiOutlineCopy className="copy" onClick={handleCopyText}/>
+        {
+            isCopied === false ? <AiOutlineCopy className="copy" onClick={handleCopyText} />
+            :
+            <AiFillCopy className="copy" />
+        }
     </div>
   )
 }
