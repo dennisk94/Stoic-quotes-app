463aaad2662da6b7780b88d223316949d7df097c
Dennis Kim <dennis.kim@hotmail.com>
2022-09-19 21:00:04 -0700

    add single author quotes page component

---
+++ src/components/SingleAuthorQuotes.js
@@ -0,0 +1,18 @@
+import { handleThumbnail } from "../util/handleThumbnail"
+
+const SingleAuthorQuotes = ( { quote } ) => {
+    const { id, author_id, author, body } = quote;
+  return (
+    <figure className="single-author-quote-card">
+        { handleThumbnail( author_id ) }
+        <blockquote className="single-author-quote">
+            { body }
+        </blockquote>
+        <figcaption className="single-author-attribution">
+            { author }
+        </figcaption>
+    </figure>
+  )
+}
+
+export default SingleAuthorQuotes
--- src/pages/SingleAuthorPage.js
+++ src/pages/SingleAuthorPage.js
@@ -1,5 +1,8 @@
 import { useParams, } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import SingleAuthorQuotes from '../components/SingleAuthorQuotes';
 
 const SingleAuthorPage = () => {
     const [ singleAuthorQuotes, setSingleAuthorQuotes ] = useState(null);
@@ -7,21 +10,37 @@ const SingleAuthorPage = () => {
     console.log(slug);
     useEffect(() => {
         const fetchAuthorQuotes = async () => {
-            // const res = await fetch( `https://stoicquotesapi.com/v1/api/quotes?page=${ generateRandomPage() }` ); 
-            // const res = await fetch('https://stoicquotesapi.com/v1/api/quotes'); 
-            // const res = await fetch('https://stoicquotesapi.com/v1/api/quotes/random');
             const res = await fetch(`https://stoicquotesapi.com/v1/api/quotes/${ slug }`);
             let data = await res.json();
             setSingleAuthorQuotes(data.data);
         }
         fetchAuthorQuotes();
     }, [slug]);
+    const settings = {
+        autoPlay:true,
+        interval: 15000,
+        transitionTime: 1350,
+        infiniteLoop:true, 
+        dynamicHeight:true, 
+        showArrows:false, 
+        showStatus:false, 
+        showIndicators:false, 
+        showThumbs:false,
+        emulateTouch:true,
+        useKeyboardArrows:true,
+        ariaLabel:'carousel',
+    }
     console.log(singleAuthorQuotes);
-  return (
+return (
     <div className="single-author-page">
-
+        <Carousel {...settings}>
+            { singleAuthorQuotes !== null ? singleAuthorQuotes.map( ( quote, i ) => { return (
+                        <SingleAuthorQuotes quote={quote} key={i}/>
+            )} ) 
+            : <h1>No quotes found...</h1>}
+        </Carousel>
     </div>
-  )
+)
 }
 
 export default SingleAuthorPage
