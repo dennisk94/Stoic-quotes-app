6a2964dc37d6679b98db7f713a72f8bd8f1af9ff
Dennis Kim <dennis.kim@hotmail.com>
2022-09-19 19:03:16 -0700

    add AuthorModal component to Author component

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import AuthorModal from "./AuthorModal";
+import { BsInfoCircle } from "react-icons/bs"; // Info Icon
 
 const Author = ( { author } ) => {
+    const [ isOpen, setIsOpen ] = useState(false);
     const { id, 
             name,
             slug,
@@ -10,15 +14,22 @@ const Author = ( { author } ) => {
             biography,
             externalResource
         } = author;
+    const handleInfoClick = () => {
+        setIsOpen(!isOpen);
+    }
   return (
-    <div className="author-card">
-        <img src={ thumbnail } alt="thumbnail" className="thumbnail"/>
-        <div className="stoic-name">
-            <p>
-                { name }
-            </p>
+    <>
+        <div className="author-card">
+            <img src={ thumbnail } alt="thumbnail" className="thumbnail"/>
+            <div className="stoic-name">
+                <p>
+                    { name }
+                </p>
+            </div>
+            <BsInfoCircle className="info" onClick={ handleInfoClick }/>
+            <AuthorModal isOpen={ isOpen } setIsOpen={ setIsOpen } author={ author }/>
         </div>
-    </div>
+    </>
   )
 }
 
