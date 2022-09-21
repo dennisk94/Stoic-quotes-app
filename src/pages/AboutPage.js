a5e97406e22eaefc70fdba6910179c6c7d7d5085
Dennis Kim <dennis.kim@hotmail.com>
2022-09-20 17:44:12 -0700

    add about page route and component

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,17 @@
+
+const AboutPage = () => {
+  return (
+    <div className="about-page">
+        <div className="about-content-wrapper">
+            <p className="about-paragraph">
+                The goal of this application is to provide curious individuals or practitioners of the Stoic philosophy a space to explore the knowledge and wisdom Stoicism offers. This app is intended for anyone, but particularly for those who lack self-esteem and/or experience heightened levels of anxiety. 
+            </p>
+            <p className="about-why">
+                The inspiration for this app stems from my own personal struggles with anxiety and self-esteem. The stoic philosophy has helped me bounce back from some of the experiences I went through since the initial Covid-19 shutdown. 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutPage
